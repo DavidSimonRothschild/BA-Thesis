0e7ab8d233a41e474f66af531c662c76da7a74bb
DavidSimonRothschild <99689774+DavidSimonRothschild@users.noreply.github.com>
2022-07-18 17:32:52 +0200

    Add files via upload
    R scripts for the analysis of text files

---
+++ Head of Scripts.R
@@ -0,0 +1,7 @@
+library(bannerCommenter)
+banner("Script name: ",
+       "Idea of Script:",
+       "Start Date: Feb 12 2022", 
+       "last modification: ",
+       "version: ",
+       emph = TRUE)
